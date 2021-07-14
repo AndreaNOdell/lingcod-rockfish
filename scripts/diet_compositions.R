5acab0c1feb0720211b953c37b16fff06f735850
Andrea Odell <anodell@ucdavis.com>
2021-07-13 18:05:05 -0700

    create vector of diet composition for each age for male and female

--- scripts/diet_compositions.R
+++ scripts/diet_compositions.R
@@ -2,7 +2,6 @@ library(tidyverse)
 library(gridExtra)
 library(mgcv)
 
-
 # Load in ONE dataset to use
 # df <- miceadds::load.Rdata2(filename="cleaned_data/lingcod_rockfish_CA.Rdata")
 
@@ -61,8 +60,7 @@ gut_summary = df %>%
             n = n())
 gut_summary[is.na(gut_summary)] = 0
 
-
-
+# plot out diet fractions
 female_dietcomp = as.data.frame(gut_summary) %>% 
   filter(Sex == "F") %>% 
   ggplot() +
@@ -85,33 +83,47 @@ male_dietcomp =  as.data.frame(gut_summary) %>%
 
 
 grid.arrange(female_dietcomp, male_dietcomp, ncol=2)
-
-gut_summary$Bin = as.numeric(gut_summary$Bin)
  
 # pdf('plots/male_dietcomp_CAonly.pdf')
 # male_dietcomp
 # dev.off()
 
+### Fitting a GAM ####
+# to the un-weighted mean of diet composition across length bins.
+
+# must convert bin from categorical to numerical since gams need numerical values.
+gut_summary$Bin = gsub("\\-.*","\\", gut_summary$Bin)
+gut_summary$Bin = as.numeric(gut_summary$Bin) + 0.5*bin_size # Set bin column to be the midpoint of each bin
+
+# female
 diet_f = as.data.frame(gut_summary) %>% 
   filter(Sex == "F") 
 diet_model_f = gam(mean ~ s(Bin), data = diet_f, method = "REML")
+plot(diet_model_f,pages=1,residuals=TRUE,all.terms=TRUE,shade=TRUE,shade.col=2)
 
+#male
 diet_m = as.data.frame(gut_summary) %>% 
   filter(Sex == "M") %>% 
   filter(mean < .24) # removed the outlier
 diet_model_m = gam(mean ~ s(Bin), data = diet_m, method = "ML")
-
-plot(diet_model_f,pages=1,residuals=TRUE,all.terms=TRUE,shade=TRUE,shade.col=2)
 plot(diet_model_m,pages=1, residuals=TRUE,all.terms=TRUE,shade=TRUE,shade.col=2)
 
 
+#### create a vector of diet composition for age ###
+load(file = "cleaned_data/lingcod_parms.Rdata")
+lingcod_female = as.data.frame(lingcod$length.at.age["female",])
+colnames(lingcod_female) = "Bin"
+diet_comp_f = predict.gam(diet_model_f, lingcod_female)
+
+lingcod_male = as.data.frame(lingcod$length.at.age["male",])
+colnames(lingcod_male) = "Bin"
+diet_comp_m = predict.gam(diet_model_m, lingcod_male)
+
+plot(diet_comp_f)
+plot(diet_comp_m)
 
 
-
-
-
-
-
+#### Other stuff that isn't super important... ####
 
 # The proportion of stomachs that contained rockfish of all the stomachs
 lingcod_rockfish_CA_F <- lingcod_rockfish_CA %>% # filter out only Females
